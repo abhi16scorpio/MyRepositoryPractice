472bcb94b5f314a5a6ac8142b006d8a154ca2836
Abhishek <Abhishek@Abhishek-PC>
2019-08-25 16:40:49 +0530

    I have committed the code successfully to github

---
+++ Internationalization_Project/src/Internationalization_Class1.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+
+public class Internationalization_Class1 
+{
+
+	public static void main(String[] args) 
+	{
+	
+	// The object of Locale class is used to get the location specific time, currency, language
+		
+		System.out.println("Code committed on git..");
+		
+	}
+
+}
